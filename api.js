c09a07365a0068d7f987b78831d6d37e81fa9818
Simon Sun <blackghost0118@gmail.com>
2016-06-27 15:22:12 +0800

    add error handlers

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const controllers = require('./controllers');
 const middlewares = require('./middlewares');
+const errorHandles = require('./errorHandles');
 
 let app = express();
 
@@ -10,5 +11,8 @@ app.use(middlewares(app));
 
 // controllers
 app.use(controllers(app));
+
+// errorHandles
+app.use(errorHandles(app));
 
 module.exports = app;

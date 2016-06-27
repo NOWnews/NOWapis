30533faaa6365a22902ef8de4390d8a720241cd0
Simon Sun <blackghost0118@gmail.com>
2016-06-27 15:09:08 +0800

    add middlewares

--- api.js
+++ api.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
 const controllers = require('./controllers');
+const middlewares = require('./middlewares');
 
 let app = express();
+
+// middlewares
+app.use(middlewares(app));
 
 // controllers
 app.use(controllers(app));

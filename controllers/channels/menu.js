6341d98d92c202e608cf2dd7023f9752deb5e856
Simon Sun <blackghost0118@gmail.com>
2016-06-27 18:39:49 +0800

    特輯選單與特輯列表

---
+++ controllers/channels/menu.js
@@ -0,0 +1,49 @@
+import co from 'co';
+import Promise from 'bluebird';
+import mongodb from 'mongodb';
+import _ from 'lodash';
+
+const debug = require('debug')('NOWapis:controller:channels:menu');
+
+const config = require('../../config');
+
+const MongoDB = Promise.promisifyAll(mongodb);
+const MongoClient = Promise.promisifyAll(MongoDB.MongoClient);
+
+module.exports = function(req, res, next) {
+
+    co(function*() {
+
+        let db = yield MongoClient.connectAsync(config.newsMongoDb);
+
+        let mainpage = yield db.collection('fields_current.node').findOne({
+            _bundle: 'mainpage'
+        });
+
+        let channelIds = _.map(mainpage.field_collection, function(collection) {
+            return collection.target_id;
+        });
+
+        let channels = yield db.collection('fields_current.node').find({
+            _id: { $in: channelIds },
+            'field_release_status.value': 1
+        })
+        .sort({
+            'field_homepos.value': -1
+        })
+        .toArrayAsync();
+
+        let menu = _.map(channels, function(channel) {
+            debug('channel = %j', channel);
+            return {
+                name: channel.title,
+                nodeId: channel._id
+            };
+        });
+
+        debug('menu = %j', menu);
+
+        return res.json(menu);
+    })
+    .catch(next);
+};
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 
 const test = require('./test');
 const news = require('./news');
+const channels = require('./channels');
 
 module.exports = function(app) {
 
     app.use('/', test);
     app.use('/', news);
+    app.use('/', channels);
 
     return function(req, res, next) {
         return next();

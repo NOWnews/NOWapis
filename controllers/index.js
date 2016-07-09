37cf53048e9e52cc46c028255c7a2e8836e544eb
Simon Sun <blackghost0118@gmail.com>
2016-07-09 23:36:24 +0800

    加入 instant 即時端點

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const news = require('./news');
 const channels = require('./channels');
 const category = require('./category');
 const headline = require('./headline');
+const instant = require('./instant');
 
 module.exports = function(app) {
 
@@ -12,6 +13,7 @@ module.exports = function(app) {
     app.use('/', channels);
     app.use('/', category);
     app.use('/', headline);
+    app.use('/', instant);
 
     return function(req, res, next) {
         return next();

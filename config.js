e97ff199eb0ef590778842b89bd57ee154855065
Simon Sun <blackghost0118@gmail.com>
2016-08-31 15:02:48 +0800

    加入 mongodb secondary config

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
      */
     // newsMongoDb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@192.168.10.181,192.168.10.182,192.168.10.183,192.168.10.184/production' : 'mongodb://nowproduction:werocks@mongodb16.nownews.com.tw,mongodb15.nownews.com.tw,mongodb14.nownews.com.tw,mongodb18.nownews.com.tw/production',
     // newsMongoDb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@192.168.10.182/production' : 'mongodb://nowproduction:werocks@192.168.10.182/production',
-    newsMongoDb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@TXGmongo-s185.nownews.com.tw,mongodb-s14.nownews.com.tw/production?readPreference=nearest&replicaSet=nowgarden' : 'mongodb://nowproduction:werocks@61.220.58.2/production',
+    newsMongoDb: process.env.NODE_ENV === 'production' ? 'mongodb://nowproduction:werocks@TXGmongo-s185.nownews.com.tw,mongodb16.nownews.com.tw,mongodb15.nownews.com.tw,mongodb18.nownews.com.tw,mongodb-s14.nownews.com.tw/production?readPreference=nearest&replicaSet=nowgarden' : 'mongodb://nowproduction:werocks@61.220.58.2/production',
 
     /*
      * mongoDB 資料

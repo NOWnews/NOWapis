f7b78187e9476682ba793e44c52b6ba31da85afb
Simon Sun <blackghost0118@gmail.com>
2016-09-13 11:11:33 +0800

    多撈五則新聞，為了原生廣告

--- middlewares/baseQuery.js
+++ middlewares/baseQuery.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
         let skip = parseInt(req.query.skip, 10);
         let page = parseInt(req.query.page, 10);
 
-        req.query.limit = Number.isNaN(limit) ? 21 : Math.max(0, limit);
+        req.query.limit = Number.isNaN(limit) ? 26 : Math.max(0, limit);
         req.query.skip = Number.isNaN(skip) ? 0 : Math.max(0, skip);
         req.query.page = Number.isNaN(page) ? 1 : Math.max(1, page);
 

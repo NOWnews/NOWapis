381f6b3b9c1e3866ccbf1cc3a5d2f8d0f0f13ea5
wayne1025 <wayne1025.tw@hotmail.com>
2017-03-10 11:33:07 +0800

    🐛 Fix: remove console.log

--- cronjobs/updateCategory.js
+++ cronjobs/updateCategory.js
@@ -20,7 +20,6 @@ module.exports = co.wrap(function*() {
     categories = _.filter(categories, (category) => {
         return (category._id !== 449377) && (category._id !== 419897) && (category._id !== 419896);
     });
-    console.log(categories,'L23');
     debug('categories = %j', categories);
 
     let cacheCategory = yield redis.setValue('categories', categories, 3600 * 24);

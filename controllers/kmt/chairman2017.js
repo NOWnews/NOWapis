7d78b2f90bc98251ae4de5d96495df895b0e17f0
Simon Sun <blackghost0118@gmail.com>
2017-04-27 18:33:35 +0800

    上吳敦義直撥

--- controllers/kmt/chairman2017.js
+++ controllers/kmt/chairman2017.js
@@ -12,15 +12,15 @@ module.exports = function(req, res, next) {
     co(function*() {
 
         return res.json({
-            title: '直播 /「龍談大小事」- 藍營準黨魁大PK',
-            teaserTitle: '19:00 「龍談大小事」- 藍營準黨魁大PK',
-            url: 'https://www.youtube.com/embed/Ps1SpLK-pqw',
+            title: '直播 /「龍談大小事」- 國民黨準黨魁吳敦義直播專訪',
+            teaserTitle: '19:00 「龍談大小事」- 國民黨準黨魁吳敦義直播專訪',
+            url: 'https://www.youtube.com/embed/7CfgWE8f2rU',
             wowza: 'http://59.124.93.43/live/KMT.stream/playlist.m3u8?pf=mm',
-            youtubeId: 'Ps1SpLK-pqw',
-            livePage: 'https://m.nownews.com/live/Ps1SpLK-pqw',
+            youtubeId: '7CfgWE8f2rU',
+            livePage: 'https://m.nownews.com/live/7CfgWE8f2rU',
             background: 'https://legacy.nownews.com/NOWnews_static/live-background.jpg',
-            isOnAir: false,
-            campainStatus: false
+            isOnAir: true,
+            campainStatus: true
         });
     })
     .catch(next);

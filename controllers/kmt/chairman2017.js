9dfdb8be855bdd7e3d6dc825699f74ff745e685c
Simon Sun <blackghost0118@gmail.com>
2017-05-02 10:18:13 +0800

    打開 5/4 龍談大小事直撥預告

--- controllers/kmt/chairman2017.js
+++ controllers/kmt/chairman2017.js
@@ -12,15 +12,15 @@ module.exports = function(req, res, next) {
     co(function*() {
 
         return res.json({
-            title: '直播 /「龍談大小事」- 國民黨準黨魁吳敦義直播專訪',
-            teaserTitle: '19:00 「龍談大小事」- 國民黨準黨魁吳敦義直播專訪',
+            title: '5/4(四) 19:00收看「龍談大小事」， 唐湘龍潘維剛跟你面對面！',
+            teaserTitle: '5/4(四) 19:00收看「龍談大小事」， 唐湘龍潘維剛跟你面對面！',
             url: 'https://www.youtube.com/embed/7CfgWE8f2rU',
             wowza: 'http://59.124.93.43/live/KMT.stream/playlist.m3u8?pf=mm',
             youtubeId: '7CfgWE8f2rU',
             livePage: 'http://dragon.nownews.com/',
             background: 'https://legacy.nownews.com/NOWnews_static/live-background.jpg',
             isOnAir: false,
-            campainStatus: false
+            campainStatus: true
         });
     })
     .catch(next);

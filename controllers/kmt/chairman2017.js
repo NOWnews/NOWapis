8bf23d9a4b2751d6c60a525a4e51d9abc835a85c
Simon Sun <blackghost0118@gmail.com>
2017-05-08 11:03:12 +0800

    改成音樂會直撥資訊

--- controllers/kmt/chairman2017.js
+++ controllers/kmt/chairman2017.js
@@ -11,14 +11,32 @@ module.exports = function(req, res, next) {
 
     co(function*() {
 
+        // 龍談大小事直撥
+        // return res.json({
+        //     title: '5/4(四) 19:00收看「龍談大小事」， 唐湘龍潘維剛跟你面對面！',
+        //     teaserTitle: '5/4(四) 19:00收看「龍談大小事」， 唐湘龍潘維剛跟你面對面！',
+        //     banner: 'http://legacy.nownews.com/NOWnews_static/live-banner.jpg',
+        //     alt: '黨主席選前直播',
+        //     url: 'https://www.youtube.com/embed/i7bPBqCZ6As',
+        //     wowza: 'http://59.124.93.43/live/KMT.stream/playlist.m3u8?pf=mm',
+        //     youtubeId: 'i7bPBqCZ6As',
+        //     livePage: 'http://dragon.nownews.com/',
+        //     background: 'https://legacy.nownews.com/NOWnews_static/live-background.jpg',
+        //     isOnAir: false,
+        //     campainStatus: false
+        // });
+
+        // 音樂會
         return res.json({
-            title: '5/4(四) 19:00收看「龍談大小事」， 唐湘龍潘維剛跟你面對面！',
-            teaserTitle: '5/4(四) 19:00收看「龍談大小事」， 唐湘龍潘維剛跟你面對面！',
-            url: 'https://www.youtube.com/embed/i7bPBqCZ6As',
-            wowza: 'http://59.124.93.43/live/KMT.stream/playlist.m3u8?pf=mm',
-            youtubeId: 'i7bPBqCZ6As',
-            livePage: 'http://dragon.nownews.com/',
-            background: 'https://legacy.nownews.com/NOWnews_static/live-background.jpg',
+            title: '今天晚上19:30 NOWnews線上直播「最美的聲音」音樂會',
+            teaserTitle: '今天晚上19:30 NOWnews線上直播「最美的聲音」音樂會',
+            banner: '',
+            alt: '',
+            url: '',
+            wowza: '',
+            youtubeId: '',
+            livePage: '',
+            background: '',
             isOnAir: false,
             campainStatus: false
         });
